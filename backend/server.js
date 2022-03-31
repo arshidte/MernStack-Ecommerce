5b988b8fd556fc7633f7e6234c3089ec1da7b84d
arshidte <arshiddiyan.te.adte@gmail.com>
2022-03-31 08:47:50 +0530

    third time

--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,52 @@
-import path from 'path'
-import express from 'express';
-import dotenv from 'dotenv';
-import { notFound,errorHandler } from './middleware/errorMiddleware.js';
-import connectDB from './config/db.js';
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
 
 //routes
-import productRoutes from './routes/productRoutes.js';
-import userRoutes from './routes/userRoutes.js';
-import orderRoutes from './routes/orderRoutes.js';
-import uploadRoutes from './routes/uploadRoutes.js';
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 
 dotenv.config();
 
-connectDB()
+connectDB();
 
 const app = express();
 app.use(express.json());
 
-app.use('/api/products',productRoutes);
-app.use('/api/users',userRoutes);
-app.use('/api/orders',orderRoutes);
-app.use('/api/upload',uploadRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
 
-app.get('/api/config/paypal', (req,res)=>{
-    res.send(process.env.PAYPAL_CLIENT_ID)
-})
+app.get("/api/config/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID);
+});
 
-const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-app.use(notFound)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-app.use(errorHandler)
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
-const port = process.env.PORT || 5000
-const node_env = process.env.NODE_ENV
-app.listen(port,()=>{
-    console.log(`Server is running ${node_env} in localhost ${port}`);
-})
+app.use(notFound);
+
+app.use(errorHandler);
+
+const port = process.env.PORT || 5000;
+const node_env = process.env.NODE_ENV;
+app.listen(port, () => {
+  console.log(`Server is running ${node_env} in localhost ${port}`);
+});
